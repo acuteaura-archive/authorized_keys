c9a834adf3fc981fe2214387e8594cf89cebd3a8
Aurelia <aurelia@schittler.dev>
2020-12-10 19:23:36 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -10,14 +10,8 @@ terraform {
       source = "hetznercloud/hcloud"
     }
   }
-  backend "s3" {
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    endpoint                    = "https://ams3.digitaloceanspaces.com"
-    region                      = "us-east-1"
+  backend "gcs" {
     bucket                      = "aura-cfg"
-    key                         = "terraform/authorized_keys.tfstate"
-    acl                         = "private"
   }
   required_version = ">= 0.13"
 }
