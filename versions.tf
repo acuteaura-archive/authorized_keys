7fe798acbe6a2a9c6ed8e6ffecf8b1d8a38d703b
Aurelia <aurelia@schittler.dev>
2020-12-10 19:31:04 +0100

    Move to Google Cloud Storage (#8)
    * Update terraform.yml
    
    * Update versions.tf
    
    * gcs

--- versions.tf
+++ versions.tf
@@ -9,15 +9,12 @@ terraform {
     hcloud = {
       source = "hetznercloud/hcloud"
     }
+    vultr = {
+      source = "vultr/vultr"
+    }
   }
-  backend "s3" {
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    endpoint                    = "https://ams3.digitaloceanspaces.com"
-    region                      = "us-east-1"
-    bucket                      = "aura-cfg"
-    key                         = "terraform/authorized_keys.tfstate"
-    acl                         = "private"
+  backend "gcs" {
+    bucket = "aura-cfg"
   }
   required_version = ">= 0.13"
 }
