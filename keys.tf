cc69bd85ed0710cc9118b6ffda9a95194c80babb
Aurelia Schittler <aurelia@schittler.dev>
2021-02-25 22:26:24 +0100

    chore: update providers

--- keys.tf
+++ keys.tf
@@ -1,5 +1,3 @@
-
-
 terraform {
   backend "gcs" {
     bucket = "tfstate-authorized-keys"
--- versions.tf
+++ versions.tf
@@ -1,16 +1,10 @@
 terraform {
   required_providers {
-    digitalocean = {
-      source = "digitalocean/digitalocean"
-    }
     github = {
-      source = "hashicorp/github"
+      source = "integrations/github"
     }
     hcloud = {
       source = "hetznercloud/hcloud"
-    }
-    vultr = {
-      source = "vultr/vultr"
     }
   }
   required_version = ">= 0.13"
